67f9e6c218f21e27125d387faf9f5e3269d4afc0
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-30 18:32:50 +0500

    Update AlgorithmComplexity.java

--- src/main/java/com/epam/mjc/stage0/AlgorithmComplexity.java
+++ src/main/java/com/epam/mjc/stage0/AlgorithmComplexity.java
@@ -20,27 +20,27 @@ public class AlgorithmComplexity {
      * Return the linear search complexity if the element you are looking for is at the end of the array.
      */
     public Complexity badLinerSearch() {
-
+        Complexity.O_N  
     }
 
     /**
      * Return the complexity of accessing an array element by index.
      */
     public Complexity arrayIndexItemAccess() {
-
+        Complexity.O_1
     }
 
     /**
      * Return the complexity of a binary search.
      */
     public Complexity binarySorting() {
-
+        Complexity.O_LOG_N   
     }
 
     /**
      * Return the complexity of the twoCycleSorting algorithm.
      */
     public Complexity twoCycleSorting() {
-
+        Complexity.O_N_LOG_N     
     }
 }
